58efedbda1ea75c8b03524d837aa543ef7150782
charifa11 <33242422+charifa11@users.noreply.github.com>
2017-11-10 10:47:05 +0800

    Add files via upload

---
+++ test2.go
@@ -0,0 +1,7 @@
+package packageone
+
+import "fmt"
+
+func testingfunction(){
+	fmt.Println("inside package one test 2")
+}
---
+++ test2main.go
@@ -0,0 +1,9 @@
+package main 
+
+import ("fmt"
+"github.com/charifa11/test/packageone"
+)
+
+func main() {
+	packageone.Testingfucntion()
+}
